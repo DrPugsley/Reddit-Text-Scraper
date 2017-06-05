f3cffcea33657a1044e99b7daca14233115b912c
Abradolf Lincler <abradolf@lincler>
2017-06-05 12:40:38 -0400

    Updated the script to include arguments, css, and better organization

--- Reddit-Text-Scraper.py
+++ Reddit-Text-Scraper.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 import praw, os, sys, datetime, argparse, ast
 
+# Setting up Argparser for commandline arguments
 parser = argparse.ArgumentParser(description='Downloads text data from a subreddit of your choice')
 parser.add_argument("-s", "--subreddit", help="Choose a subreddit to download", action="store")
 parser.add_argument("-st", "--sort", help="Choose whether to download hot, new, or top. If unused, will get new", action="store")
@@ -10,7 +11,7 @@ args = parser.parse_args()
 getcomments = args.nocomments
 numberofposts = args.posts
 sortby = args.sort
-if not numberofposts is None:
+if numberofposts is not None:
 	numberofposts = int(numberofposts)
 
 # Setting Variables
@@ -34,82 +35,101 @@ postslist = open(subname+' Post Archive.html', "a")
 archive = """<html>
 <LINK REL=StyleSheet HREF=\""""+subname+'/'"""style.css" TYPE="text/css">
 <center><head>Posts from /r/"""+subname+"""</head></center>
-<body>\n"""
+<body> <br> <br>\n"""
 postslist.write(archive)
 
+# Function for downloading and writing posts and comments
+def dothestuff():
+	# Makes an html file for the current post and sets the file's name to the post's id
+	savepost = open(os.path.join(subname, submission.id+'.html'), "a")
+
+	# Increases the index of posts downloaded by 1. Used to display "Saving Post #___..." in console
+	global postnumber
+	postnumber+= 1
+	print('Saving Post #'+str(postnumber)+': '+submission.title)
+
+	# Setting variable to be added to the submission's html file (The ".replace"s fix some errors that make the browser unable to read certain characters properly)
+	if submission.selftext_html is not None:
+		post = """<html>
+<link rel=StyleSheet href="style.css" type="text/css">
+<head></head>
+		<body> \n<p>"""+submission.title.replace("’","'").replace('”','"').replace("—","--").replace('“','"').replace("‘","'").replace("…","...").replace("–","-")+"""</p> <p>"""+submission.selftext_html.replace("’","'").replace('”','"').replace("—","--").replace('“','"').replace("‘","'").replace("…","...").replace("–","-")+"""</p><br><center>Comments</center><br><br>"""
+	else:
+		post = """<html>
+<link rel=StyleSheet href="style.css" type="text/css">
+<head></head>
+<body> \n<p>"""+submission.title+"""</p> <p>"""+submission.selftext.replace("’","'").replace('”','"').replace("—","--").replace('“','"').replace("‘","'").replace("…","...").replace("–","-")+"""</p><br><center>Comments</center><br><br>"""
+	# Writing current post's title and content to its html file
+	savepost.write(post)
+	# Checking whether "-nc" argument was used, downloads comments if not
+	if getcomments:
+		# Sets comments to sort by top rather than new
+		submission.comment_sort = 'top'
+		# Gets rid of "More comments" attribute error by not trying to download those comments
+		submission.comments.replace_more(limit=0)
+		# Gets comments for the current submission
+		for comment in submission.comments:
+			comment = """<p><b>"""+'['+str(comment.score)+' Points] '+str(comment.author)+': '+"""</b>"""+comment.body_html+"""</p> <br>"""
+			savepost.write(comment)
+	
+	# Closes the html tags for the current submission's file
+	finish = """ </body> </html>"""
+	savepost.write(finish)
+	savepost.close()
+	# Gets the post's author, score, number of comments, date, and title to be added to the main html file, then adds them
+	currentpost = """ <div class="postinfo"> """+str(submission.author)+' ['+str(submission.score)+' points] '+'['+str(submission.num_comments)+' comments] '+str(getdate(submission))+' '+""" <br> <a href=\""""+subname+'/'+submission.id+'.html'+'">'+submission.title.replace("’","'").replace('”','"').replace("—","--").replace('“','"').replace("‘","'").replace("…","...").replace("–","-")+'</a> </div> <br>\n\n'""""""
+	postslist.write(currentpost)
+
 # Gets and downloads submissions and comments, then adds them to the "Archive" html file in the current directory
-if not sortby is None:
+if sortby is not None:
 	for submission in getattr(reddit.subreddit(subname), sortby)(limit=numberofposts):
-		# Makes an html file for the current post and sets the file's name to the post's id
-		savepost = open(os.path.join(subname, submission.id+'.html'), "a")
-
-		# Increases the index of posts downloaded by 1. Used to display "Saving Post #___..." in console
-		postnumber+=1
-		print('Saving Post #'+str(postnumber)+': '+submission.title.replace("/","."))
-
-		# Setting variable to be added to the submission's html file (The ".replace"s fix some errors that make the browser unable to read certain characters properly)
-		if not submission.selftext_html is None:
-			post = """<html>
-			<head></head>
-			<body> \n<p>"""+submission.title+"""</p> <p>"""+submission.selftext_html.replace("’","'").replace('”','"').replace("—","--").replace('“','"').replace("‘","'").replace("…","...").replace("–","-")+"""</p><br><center>Comments</center><br><br>"""
-		else:
-			post = """<html>
-			<head></head>
-			<body> \n<p>"""+submission.title+"""</p> <p>"""+submission.selftext.replace("’","'").replace('”','"').replace("—","--").replace('“','"').replace("‘","'").replace("…","...").replace("–","-")+"""</p>"""
-		savepost.write(post)
-		if getcomments:
-			# Sets comments to sort by top rather than new
-			submission.comment_sort = 'top'
-			# Gets rid of "More comments" attribute error by not trying to download those comments
-			submission.comments.replace_more(limit=0)
-			# Gets comments for the current submission
-			for comment in submission.comments:
-				comment = """<p><b>"""+'['+str(comment.score)+' Points] '+str(comment.author)+': '+"""</b>"""+comment.body_html+"""</p>"""
-				savepost.write(comment)
-		# Closes the html tags for the current submission's file
-		finish = """ </body> </html>"""
-		savepost.write(finish)
-		savepost.close()
-		# Gets the post's author, score, number of comments, date, and title to be added to the main html file, then adds them
-		currentpost = """ """+str(submission.author)+' ['+str(submission.score)+' points] '+'['+str(submission.num_comments)+' comments] '+str(getdate(submission))+' '+"""<a href=\""""+subname+'/'+submission.id+'.html'+'">'+submission.title+'</a> <br>\n\n'""""""
-		postslist.write(currentpost)
+		dothestuff()
 else:
 	for submission in reddit.subreddit(subname).submissions():
-		# Makes an html file for the current post and sets the file's name to the post's id
-		savepost = open(os.path.join(subname, submission.id+'.html'), "a")
-
-		# Increases the index of posts downloaded by 1. Used to display "Saving Post #___..." in console
-		postnumber+=1
-		print('Saving Post #'+str(postnumber)+': '+submission.title.replace("/","."))
-
-		# Setting variable to be added to the submission's html file (The ".replace"s fix some errors that make the browser unable to read certain characters properly)
-		if not submission.selftext_html is None:
-			post = """<html>
-			<head></head>
-			<body> \n<p>"""+submission.title+"""</p> <p>"""+submission.selftext_html.replace("’","'").replace('”','"').replace("—","--").replace('“','"').replace("‘","'").replace("…","...").replace("–","-")+"""</p>"""
-		else:
-			post = """<html>
-			<head></head>
-			<body> \n<p>"""+submission.title+"""</p> <p>"""+submission.selftext.replace("’","'").replace('”','"').replace("—","--").replace('“','"').replace("‘","'").replace("…","...").replace("–","-")+"""</p>"""
-		savepost.write(post)
-		if getcomments:
-			# Sets comments to sort by top rather than new
-			submission.comment_sort = 'top'
-			# Gets rid of "More comments" attribute error by not trying to download those comments
-			submission.comments.replace_more(limit=0)
-			# Gets comments for the current submission
-			for comment in submission.comments:
-				comment = """<p><b>"""+'['+str(comment.score)+' Points] '+str(comment.author)+': '+"""</b>"""+comment.body_html+"""</p>"""
-				savepost.write(comment)
-		# Closes the html tags for the current submission's file
-		finish = """ </body> </html>"""
-		savepost.write(finish)
-		savepost.close()
-		# Gets the post's author, score, number of comments, date, and title to be added to the main html file, then adds them
-		currentpost = """ """+str(submission.author)+' ['+str(submission.score)+' points] '+'['+str(submission.num_comments)+' comments] '+str(getdate(submission))+' '+"""<a href=\""""+subname+'/'+submission.id+'.html'+'">'+submission.title+'</a> <br>\n\n'""""""
-		postslist.write(currentpost)
+		dothestuff()
 
 # Closes the Main html file's tags
 finished = """</body> </html>"""
 postslist.write(finished)
 postslist.close()
+cssfile = open(os.path.join(subname, 'style.css'), "a")
+csscode = """.md{
+	color: #fff;
+	background-color: #333;
+	padding-left: 10px;
+	padding-right: 10px;
+	min-height: 10em;
+	display: table-cell;
+	vertical-align: middle;
+	box-shadow: 5px 5px 10px #000;
+}
+.postinfo{
+	color: #fff;
+	background-color: #333;
+	padding-left: 10px;
+	padding-right: 10px;
+	min-height: 10em;
+	display: table-cell;
+	vertical-align: middle;
+	box-shadow: 5px 5px 10px #000;
+	padding-bottom: 10px;
+	padding-top: 5px;
+}
+body{
+background-color: #212121;
+color: #fff;
+}
+A:link {
+ color: #add8e6;
+ font-weight: bold;
+}
+A:visited {
+ color: #adbce6;
+ font-weight: bold;
+}
+A:hover {
+ color: white;
+}
+"""
+cssfile.write(csscode)
+cssfile.close()
